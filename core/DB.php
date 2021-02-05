4f1e4959bb93435282fb060b8ba800c3226d2a2a
ziovik <mondaydaniel2002@yahoo.com>
2021-02-05 13:42:21 +0300

    including DB

--- core/DB.php
+++ core/DB.php
@@ -3,5 +3,15 @@
 
 class DB
 {
+   private static $_instance = null;
+   private $_pdo, $_query, $_error = false, $_result, $_count = 0, $_lastInsertId = null;
 
+   private function __construct()
+   {
+       try {
+           $this->_pdo = new PDO('mysql:host=127.0.0.1;dbname=mvc_train', 'root', '');
+       } catch (PDOException $e) {
+           die($e->getMessage());
+       }
+   }
 }
